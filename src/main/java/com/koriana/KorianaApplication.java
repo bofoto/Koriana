803ae0ff5911d25c562eb9db08bfbc60b9478868
bofoto <cho03k@gmail.com>
2025-05-19 14:56:53 +0900

    add project files 	new file:   .classpath 	new file:   .gradle/8.13/checksums/checksums.lock 	new file:   .gradle/8.13/checksums/md5-checksums.bin 	new file:   .gradle/8.13/checksums/sha1-checksums.bin 	new file:   .gradle/8.13/executionHistory/executionHistory.lock 	new file:   .gradle/8.13/fileChanges/last-build.bin 	new file:   .gradle/8.13/fileHashes/fileHashes.lock 	new file:   .gradle/8.13/gc.properties 	new file:   .gradle/buildOutputCleanup/buildOutputCleanup.lock 	new file:   .gradle/buildOutputCleanup/cache.properties 	new file:   .gradle/vcs-1/gc.properties 	new file:   .project 	new file:   .settings/org.eclipse.buildship.core.prefs 	new file:   .settings/org.springframework.ide.eclipse.prefs 	new file:   HELP.md 	new file:   bin/main/application.properties 	new file:   bin/main/com/koriana/Configuration/DBConfiguration.class 	new file:   bin/main/com/koriana/KorianaApplication.class 	new file:   bin/test/com/koriana/KorianaApplicationTests.class 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/com/koriana/Configuration/DBConfiguration.java 	new file:   src/main/java/com/koriana/KorianaApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/koriana/KorianaApplicationTests.java

---
+++ src/main/java/com/koriana/KorianaApplication.java
@@ -0,0 +1,13 @@
+package com.koriana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koriana/KorianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koriana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
